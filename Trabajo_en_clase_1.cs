ac41717243dc6f081b007101cfce8b8bc1ce1ce3
Carlos Martinez <carlitosmar1922@gmail.com>
2024-10-15 23:00:59 -0600

    Add files via upload

---
+++ Trabajo_en_clase_1.cs
@@ -0,0 +1,13 @@
+Using sistem;
+Using System.componentModel;
+enum Propiedades{
+    [Description("Puerto 8180,Automata")]
+    Automata=8180,
+
+    
+    [Description("Puerto 8380,Automata")]
+    Automata=8380,
+    
+    [Description("Puerto 8580,Automata")]
+    Automata=8580,
+}
